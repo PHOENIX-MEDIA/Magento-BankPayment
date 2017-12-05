711ca3f826ee96ed0551836fd871dbab6708eefe
MaWoScha <manfredwormschaefer@gmail.com>
2017-12-05 23:37:26 +0100

    Move templates from 'bankpayment' to 'phoenix/bankpayment' - Version 1.2.1

--- app/code/community/Phoenix/BankPayment/Block/Info.php
+++ app/code/community/Phoenix/BankPayment/Block/Info.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Magento
  *
@@ -11,34 +10,45 @@
  *
  * @category   Mage
  * @package    Phoenix_BankPayment
- * @copyright  Copyright (c)0 2008 Andrej Sinicyn
+ * @copyright  Copyright (c) 2008 Andrej Sinicyn
  * @copyright  Copyright (c) 2010 Phoenix Medien GmbH & Co. KG (http://www.phoenix-medien.de)
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-
 
 class Phoenix_BankPayment_Block_Info extends Mage_Payment_Block_Info
 {
     protected function _construct()
     {
         parent::_construct();
-        $this->setTemplate('bankpayment/info.phtml');
+        $this->setTemplate('phoenix_bankpayment/info.phtml');
     }
 
+    /**
+     * @return string
+     */
     public function toPdf()
     {
-        $this->setTemplate('bankpayment/pdf/info.phtml');
+        $this->setTemplate('phoenix_bankpayment/pdf/info.phtml');
         return $this->toHtml();
     }
 
+    /**
+     * @return array
+     */
     public function getAccounts() {
         return $this->getMethod()->getAccounts();
     }
 
+    /**
+     * @return string
+     */
     public function getShowBankAccountsInPdf() {
         return $this->getMethod()->getConfigData('show_bank_accounts_in_pdf');
     }
 
+    /**
+     * @return string
+     */
     public function getShowCustomTextInPdf() {
         return $this->getMethod()->getConfigData('show_customtext_in_pdf');
     }
