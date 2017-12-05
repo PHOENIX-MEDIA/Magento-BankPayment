3f1e3aa2ba5edae4c212fdacae64507146921127
MaWoScha <manfredwormschaefer@gmail.com>
2017-12-05 23:37:26 +0100

    Move templates from 'bankpayment' to 'phoenix/bankpayment' and resolved merge conflicts

--- app/code/community/Phoenix/BankPayment/Block/Info.php
+++ app/code/community/Phoenix/BankPayment/Block/Info.php
@@ -15,29 +15,40 @@
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
-
 class Phoenix_BankPayment_Block_Info extends Mage_Payment_Block_Info
 {
     protected function _construct()
     {
         parent::_construct();
-        $this->setTemplate('bankpayment/info.phtml');
+        $this->setTemplate('phoenix/bankpayment/info.phtml');
     }
 
+    /**
+     * @return string
+     */
     public function toPdf()
     {
-        $this->setTemplate('bankpayment/pdf/info.phtml');
+        $this->setTemplate('phoenix/bankpayment/pdf/info.phtml');
         return $this->toHtml();
     }
 
+    /**
+     * @return array
+     */
     public function getAccounts() {
         return $this->getMethod()->getAccounts();
     }
 
+    /**
+     * @return string
+     */
     public function getShowBankAccountsInPdf() {
         return $this->getMethod()->getConfigData('show_bank_accounts_in_pdf');
     }
 
+    /**
+     * @return string
+     */
     public function getShowCustomTextInPdf() {
         return $this->getMethod()->getConfigData('show_customtext_in_pdf');
     }
